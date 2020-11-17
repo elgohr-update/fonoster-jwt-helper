3bb3d391f638cdda1bf2d94c2ba979e4ae2f1f3f
Pedro Sanders <sanderspedro@gmail.com>
2020-11-16 20:21:28 -0500

    Added print function

--- jwthelper.js
+++ jwthelper.js
@@ -5,18 +5,30 @@ const { waitFile } = require('wait-file')
 const { join } = require('path')
 const { homedir } = require('os')
 const { copy } = require('fs-extra')
+const fs = require('fs')
 const src = join(homedir(), '.fonos')
-const dest = join(homedir(), '/certs')
+const dest = join(homedir(), '/access')
+const pathToConfig = join(src, 'config')
 
-waitFile({resources: [join(src, 'config'), join(src, 'jwt.salt')]})
+waitFile({resources: [pathToConfig, join(src, 'jwt.salt')]})
 
-async function main() {
+function printAccessInfo() {
+  const data = fs.readFileSync(pathToConfig)
+  const formatted = JSON.stringify(JSON.parse(data), undefined, 2)
+  console.log()
+  console.log('ACCESS CONFIG:')
+  console.log()
+  console.log(formatted)
+  console.log()
+}
+
+function main() {
   copy(src, dest, function (err) {
     if (err){
         console.log('An error occured while copying the folder.')
         return console.error(err)
     }
-    console.log('Certificate creation completed')
+    if (process.env.PRINT_ACCESS_INFO === 'true') printAccessInfo()
   })
 }
 
